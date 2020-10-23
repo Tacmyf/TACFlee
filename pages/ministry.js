aa3446e0f81d5c97ed256100e8c6149c264d3d02
Tacmyf <youthfellowshiptac@gmail.com>
2020-10-24 03:46:06 +0800

    ministry

--- pages/ministry.js
+++ pages/ministry.js
@@ -1,10 +1,11 @@
 import Undercons from "../components/Undercons"
+import Upcoming from "../components/Upcoming"
 import Layout from "../components/Layout"
 
 function Ministry () {
     return (
         <Layout>
-        <Undercons></Undercons>
+        <Upcoming></Upcoming>
         </Layout>
     )
 }
