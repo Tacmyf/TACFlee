d88087c4d0023d8a61fa16caf99b3564e4e71baf
MichaelDepp <michaeldepp7@gmail.com>
2020-11-11 01:00:27 +0800

    added pdf preface & finale

--- components/Content.js
+++ components/Content.js
@@ -16,7 +16,7 @@ function Content(props) {
   const sbtn = useColorModeValue("#F8F8F8", "#304FFF");
   const { ans, showAns, createMarkup, conv } = props;
 
-  const pdf = "./" + props.article.slug + ".pdf"
+  const pdf = "./" + props.article.slug + ".pdf";
 
   return (
     <>
@@ -62,8 +62,8 @@ function Content(props) {
               }
         }
       ></Box>
-      {!!props.article.answer && (
-        <>
+      <>
+        {!!props.article.answer && (
           <Center>
             <Box
               fontFamily="Poppins"
@@ -91,63 +91,63 @@ function Content(props) {
               </Center>
             </Box>
           </Center>
-          <Center>
+        )}
+        <Center>
+          <Box
+            fontFamily="Poppins"
+            cursor="pointer"
+            fontWeight="semi"
+            fontSize="lg"
+            w={["55%", "35%", "25%", "20%"]}
+            _hover={{ bg: btn }}
+            bg={btn}
+            color="#F8F8F8"
+            size="lg"
+            rounded={25}
+            mt={5}
+            pt={1}
+            pb={1}
+          >
+            <a href={pdf} download>
+              <Center>
+                <Text>{props.lang ? "Download All" : "பதிவிறக்கம்"}</Text>
+              </Center>
+            </a>
+          </Box>
+        </Center>
+        {!!ans && (
+          <Box mt={6} mb={10}>
+            <Heading>{props.lang ? "Answers" : "விடைகள்"}</Heading>
             <Box
-              fontFamily="Poppins"
-              cursor="pointer"
-              fontWeight="semi"
-              fontSize="lg"
-              w={["55%", "35%", "25%", "20%"]}
-              _hover={{ bg: btn }}
-              bg={btn}
-              color="#F8F8F8"
-              size="lg"
-              rounded={25}
-              mt={5}
-              pt={1}
-              pb={1}
-            >
-              <a href={pdf} download>
-                <Center>
-                  <Text>{props.lang ? "Download All" : "பதிவிறக்கம்"}</Text>
-                </Center>
-              </a>
-            </Box>
-          </Center>
-          {!!ans && (
-            <Box mt={6} mb={10}>
-              <Heading>{props.lang ? "Answers" : "விடைகள்"}</Heading>
-              <Box
-                mb={[6, 8, 10, 12]}
-                dangerouslySetInnerHTML={
-                  props.lang
-                    ? createMarkup(conv(props.article.answer[0]))
-                    : createMarkup(conv(props.article.answer[1]))
-                }
-                style={
-                  props.lang
-                    ? {
-                        textAlign: "justify",
-                        fontFamily: "Poppins",
-                        fontWeight: "500",
-                        fontSize: "18px",
-                        whiteSpace: "pre-line",
-                        wordBreak: "break-word",
-                      }
-                    : {
-                        textAlign: "justify",
-                        fontFamily: "Poppins",
-                        fontWeight: "500",
-                        fontSize: "18px",
-                        whiteSpace: "pre-line",
-                        wordBreak: "break-word",
-                      }
-                }
-              ></Box>
-            </Box>
-          )}
-        </>
-      )}
+              mb={[6, 8, 10, 12]}
+              dangerouslySetInnerHTML={
+                props.lang
+                  ? createMarkup(conv(props.article.answer[0]))
+                  : createMarkup(conv(props.article.answer[1]))
+              }
+              style={
+                props.lang
+                  ? {
+                      textAlign: "justify",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      fontSize: "18px",
+                      whiteSpace: "pre-line",
+                      wordBreak: "break-word",
+                    }
+                  : {
+                      textAlign: "justify",
+                      fontFamily: "Poppins",
+                      fontWeight: "500",
+                      fontSize: "18px",
+                      whiteSpace: "pre-line",
+                      wordBreak: "break-word",
+                    }
+              }
+            ></Box>
+          </Box>
+        )}
+      </>
     </>
   );
 }
