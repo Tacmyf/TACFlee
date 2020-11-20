ff6c839eb913a2c274e1adc603cd9136a3a4ebb5
MichaelDepp <michaeldepp7@gmail.com>
2020-11-21 02:13:22 +0800

    fix icon mask

--- pages/_app.js
+++ pages/_app.js
@@ -35,24 +35,6 @@ function App({ Component, pageProps }) {
   const [lessons, setLessons] = useState([]);
   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-        navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log(
-              "Service Worker registration successful with scope: ",
-              registration.scope
-            );
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
-      });
-    }
-  }, []);
-
   const ref = firebase.firestore().collection("lessons");
   function getLesson() {
     setLoading(true);
@@ -78,6 +60,10 @@ function App({ Component, pageProps }) {
           <link rel="shortcut icon" href="./ico/icon-192x192.png" />
           <link rel="apple-touch-icon" href="/example.png"></link>
           <meta name="theme-color" content="#F8F8F8" />
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
           <link rel="manifest" href="./manifest.json" />
         </Head>
         <Component {...pageProps} lessons={lessons} />
