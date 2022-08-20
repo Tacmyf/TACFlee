7b43d041a56f1517ec0cea56cf4fc241434d3b7f
MichaelDepp <michaeldepp7@gmail.com>
2022-08-20 12:40:16 +0800

    feat : created new page for the education page

---
+++ pages/education.js
@@ -0,0 +1,31 @@
+import Undercons from "../components/Undercons";
+import Layout from "../components/Layout";
+import Head from "next/head";
+
+function Education() {
+  return (
+    <>
+      <Head>
+        <title>Flee - Upcoming</title>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ET3VV11Y1F"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-ET3VV11Y1F');`,
+          }}
+        />
+      </Head>
+      <Layout>
+        <Undercons/>
+      </Layout>
+    </>
+  );
+}
+
+export default Education;
