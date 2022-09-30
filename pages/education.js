c6cb8d3412b40325d8ec3ecf70391acd078b7c82
MichaelDepp <michaeldepp7@gmail.com>
2022-09-30 10:11:41 +0800

    feat : added the component to the education page

--- pages/education.js
+++ pages/education.js
@@ -1,31 +1,29 @@
-import Undercons from "../components/Undercons";
-import Layout from "../components/Layout";
-import Head from "next/head";
+import Undercons from '../components/Undercons';
+import Layout from '../components/Layout';
+import Head from 'next/head';
+import EducationLink from '../components/EducationLink';
 
 function Education() {
-  return (
-    <>
-      <Head>
-        <title>Flee - Upcoming</title>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-ET3VV11Y1F"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
+	return (
+		<>
+			<Head>
+				<title>Flee - Upcoming</title>
+				<script async src="https://www.googletagmanager.com/gtag/js?id=G-ET3VV11Y1F"></script>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
           
             gtag('config', 'G-ET3VV11Y1F');`,
-          }}
-        />
-      </Head>
-      <>
-        <Undercons/>
-      </>
-    </>
-  );
+					}}
+				/>
+			</Head>
+			<>
+				<EducationLink />
+			</>
+		</>
+	);
 }
 
 export default Education;
