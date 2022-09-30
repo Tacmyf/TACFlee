10ed7b3d23b66aba44b2b37c7ece0ae5cb6ae1a4
MichaelDepp <michaeldepp7@gmail.com>
2022-09-30 10:31:24 +0800

    feat : added objective n grant info

--- components/EducationLink.js
+++ components/EducationLink.js
@@ -34,34 +34,83 @@ function EducationLink(props) {
 							fontFamily="heading"
 							fontSize={['lg', 'lg', 'lg', 'xl']}
 							fontWeight={400}
-							my={2}
+							mt={5}
 						>
 							{
 								'Dear applicants, we cordially invite you to apply for the TAC MYF Education Fund using this Google Form. Nevertheless, we would like to remind you that this initiative is limited to the members of the Tamil Annual Conference. Please click the button below to apply'
 							}
 						</Text>
+
+						<Text
+							color={sbtn}
+							lineHeight={'1em'}
+							textAlign="center"
+							fontFamily="heading"
+							fontSize={['lg', 'lg', 'lg', 'xl']}
+							fontWeight={600}
+							mt={5}
+						>
+							{'Grant Info'}
+						</Text>
+						<Text
+							color={sbtn}
+							lineHeight={'1em'}
+							textAlign="center"
+							fontFamily="heading"
+							fontSize={['lg', 'lg', 'lg', 'xl']}
+							fontWeight={400}
+							my={2}
+						>
+							{
+								'The TAC MYF Education Fund is aimed to benefit students from families in need. We believe that it is our social responsibility to provide financial support which leads to providing opportunities and giving chances to students to have the higher education necessary to be successful individuals and the next generation of leaders.'
+							}
+						</Text>
+						<Text
+							color={sbtn}
+							lineHeight={'1em'}
+							textAlign="center"
+							fontFamily="heading"
+							fontSize={['lg', 'lg', 'lg', 'xl']}
+							fontWeight={600}
+							mt={5}
+						>
+							{'Objective'}
+						</Text>
+						<Text
+							color={sbtn}
+							lineHeight={'1em'}
+							textAlign="center"
+							fontFamily="heading"
+							fontSize={['lg', 'lg', 'lg', 'xl']}
+							fontWeight={400}
+							my={2}
+						>
+							{
+								'To support and encourage young individuals of the Tamil Annual Conference in pursuing tertiary education.'
+							}
+						</Text>
 					</Box>
 				</Center>
 				<Center>
-					<Link href="https://forms.gle/WxLkfNXCjSX1HDVw5" isExternal>
-						<Box
-							onClick={() => console.log('Clicked')}
-							fontFamily="Poppins"
-							cursor="pointer"
-							fontWeight="regular"
-							fontSize="md"
-							_hover={{ bg: btn }}
-							bg={btn}
-							color="#F8F8F8"
-							size="lg"
-							rounded={25}
-							my={2}
-							py={1}
-							px={3}
-						>
+					<Box
+						onClick={() => console.log('Clicked')}
+						fontFamily="Poppins"
+						cursor="pointer"
+						fontWeight="regular"
+						fontSize="md"
+						_hover={{ bg: btn }}
+						bg={btn}
+						color="#F8F8F8"
+						size="lg"
+						rounded={25}
+						my={2}
+						py={1}
+						px={3}
+					>
+						<Link style={{ textDecoration: 'none' }} href="https://forms.gle/WxLkfNXCjSX1HDVw5" isExternal>
 							<Text align={'center'}>Google Form Link</Text>
-						</Box>
-					</Link>
+						</Link>
+					</Box>
 				</Center>
 			</Box>
 		</>
