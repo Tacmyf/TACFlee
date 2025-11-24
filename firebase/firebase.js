63a2662e2428578405602343893a0f68bf3a76c0
michaeldepp <michaeldepp7@gmail.com>
2025-11-24 14:23:08 +0800

    fix : env issue

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,21 +1,21 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCAgO4v-YEAqIC-g27l8mSRZQAq9o2waqc",
-  authDomain: "flee-1866b.firebaseapp.com",
-  databaseURL: "https://flee-1866b.firebaseio.com",
-  projectId: "flee-1866b",
-  storageBucket: "flee-1866b.appspot.com",
-  messagingSenderId: "1075023023383",
-  appId: "1:1075023023383:web:631621380b118061cf3b52",
-  measurementId: "G-FCM86CXX22",
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+	measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
-  console.log("=================> wokringgg");
-  firebase.initializeApp(firebaseConfig);
+	console.log('=================> wokringgg');
+	firebase.initializeApp(firebaseConfig);
 }
 
 export default firebase;
